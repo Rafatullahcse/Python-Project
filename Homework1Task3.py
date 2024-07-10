302f519648e6aef3b36e5a2b8d70374f9964ccaa
Rafat Ullah <43478473+Rafatullahcse@users.noreply.github.com>
2024-07-10 09:18:49 +0600

    Delete Homework1Task3.py

--- Homework1Task3.py
+++
@@ -1,6 +0,0 @@
-minutes = int(input("Enter the number of minutes: "))
-
-hours = minutes // 60
-remaining_minutes = minutes % 60
-
-print("{} minutes is equal to {} hours and {} minutes".format(minutes, hours, remaining_minutes))
