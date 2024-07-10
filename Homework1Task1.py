5b8955aaafb1588207d7760cb5bb69817ad86117
Rafat Ullah <43478473+Rafatullahcse@users.noreply.github.com>
2024-07-10 08:43:49 +0600

    Update Homework1Task1.py

--- Homework1Task1.py
+++ Homework1Task1.py
@@ -1,4 +1,3 @@
-# Get input from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
@@ -7,7 +6,6 @@ difference = num1 - num2
 product = num1 * num2
 quotient = num1 / num2
 
-# Print the results
 print("The sum of {} and {} is {}".format(num1, num2, sum))
 print("The difference of {} and {} is {}".format(num1, num2, difference))
 print("The product of {} and {} is {}".format(num1, num2, product))
